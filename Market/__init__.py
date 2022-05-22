e06dd64502f93ff0738f10a80d1d574e605bf97f
Tilak <tilakraj7050@gmail.com>
2022-05-22 12:20:16 +0530

    added files

---
+++ Market/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '759ac9ebc3d00276bdf7ff5e'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
+from Market import routes
